b9b1967395c79d53ed0a08ba271dc0676c8a6e64
BenWildgoose <bw809@bath.ac.uk>
2025-03-26 14:45:16 +0000

    g is working

--- MyProject/SecondLemma.lean
+++ MyProject/SecondLemma.lean
@@ -6,26 +6,30 @@ import Mathlib.Analysis.Asymptotics.Defs -- Contains the definitions for big-O.
 
 section basics
 
-variable (a b : ℝ)
+variable (a b ε: ℝ)
 
 -- Assume `a > 0` and `b > 1` inside a context
-variable (ha : 0 < a) (hb : 1 < b) --Effectively a true statement, but it will need to be called upon in proofs.
+variable (ha : 0 < a) (hb : 1 < b) (he : 0 < ε) --Effectively a true statement, but it will need to be called upon in proofs.
 
-def pos_real := { x : ℝ // 0 <= x } -- required type for T function. CHANGE THIS TO >= 1???
+def pos_real := { x : ℝ // 1 <= x } -- required type for T function. CHANGE THIS TO >= 1???
 
-def f (x: pos_real) : ℝ := x.val ^ 2 + 1 -- x.val lets lean know that x is from the pos_real domain.
+variable (f : ℝ → ℝ)-- Currently using f: ℝ → ℝ . This MIGHT cause some small issues.
 
 -- def g (n : pos_real) : ℝ := ∑ a^j * f (n/(b^j)) in Finset.range (Real.logb n)
 
 open Finset
 
-noncomputable def g (n : pos_real) : ℝ :=
+noncomputable def g (n: pos_real) : ℝ :=
   ∑ j in Finset.range ((⌊Real.logb b n.val⌋.toNat) + 1),
-    a^j * f ⟨n.val / b^j, div_nonneg n.property (pow_nonneg (le_of_lt (zero_lt_one.trans hb)) j)⟩
---g is defined as non computable as it contains logb, which could make proofs harder
+    a^j * f (n.val / b^j)
 
--- theorem L43FirstPart (ε : ℝ)
---   ( ε > 0, f isBigO )
+
+-- g is defined as non computable as it contains logb, which could make proofs harder
+-- Use g as a function from ℝ → ℝ . Then just say 'I only care about n>= 1' Same for f?
+
+
+
+
 
 
 #check @g
