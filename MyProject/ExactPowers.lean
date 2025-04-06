cd856eca479c26cbedd3204d4f49ebb241d60852
BenWildgoose <bw809@bath.ac.uk>
2025-04-06 17:19:11 +0100

    made progress on ExactPowers

--- MyProject/ExactPowers.lean
+++ MyProject/ExactPowers.lean
@@ -12,7 +12,7 @@ import Mathlib.Analysis.SpecialFunctions.Pow.Real --Loads of stuff for dealing w
 --   decreasing_by
 
 
-theorem exactPowersOfb (T : ℝ → ℝ) (f: ℝ → ℝ ) (hT1 : ∀ x , x <= 1 → T x = 1 )
+theorem exactPowersOfb (T : ℝ → ℝ) (f: ℝ → ℝ ) (hb : b > 1) (hT1 : ∀ x , x <= 1 → T x = 1 )
 (hT2 : ∀ x , x > 1 → T x = a *  T (x / b) + f x) :
   T (b ^ k) = a^k + ∑ j in Finset.range (k), (a^j * f ((b^k) / (b^j)) ) := by
   induction k with
@@ -21,6 +21,18 @@ theorem exactPowersOfb (T : ℝ → ℝ) (f: ℝ → ℝ ) (hT1 : ∀ x , x <= 1
             simp
   | succ i ih => {
     rw [hT2]
+    have h : b ^ (i + 1) / b = b * b ^ i * b⁻¹ := by ring
+    rw [h]
+    nth_rw 3 [mul_comm]
+    field_simp
+    rw [ih]
+    -- set l := i + 1 with hl --Replacing i+1 with l to fix type errors. Uses set
+    -- have hT : T (b ^ l) = a * T (b ^ l / b) + f (b ^ l) := hT2 _ sorry --Needs completing
+    -- rw [←hT]
+    rw [mul_add] --Distributes the a over the two terms on the left.
+    nth_rw 1 [mul_comm]
+    rw [← pow_succ]
+
 
   }
 
